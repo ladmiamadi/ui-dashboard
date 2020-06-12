00a367fb8de7698275e7582e2cb25879a2f13958
d <arnaud.vanderschrieck@gmail.com>
2020-06-12 08:42:18 +0200

    fix CR correction

--- src/talents/helpers/talentFilter.tsx
+++ src/talents/helpers/talentFilter.tsx
@@ -2,7 +2,9 @@ import { UserProfile } from '../../app/index';
 
 export class TalentUserProfilesFilter {
   public static filterByEnvironment(profile: UserProfile[], environment: 'live' | 'working') {
-    return { ...profile.filter((profile) => profile.environment === environment)[0] };
+    const environement = { ...profile.filter((profile) => profile.environment === environment)[0] };
+
+    return environement;
   }
 }
 
