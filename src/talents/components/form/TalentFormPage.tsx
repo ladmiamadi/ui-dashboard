c22fd5fa479ac2444e9f5958de1942ca97604bfa
d <arnaud.vanderschrieck@gmail.com>
2020-06-18 14:32:14 +0200

    rename TalentForm.css

--- src/talents/components/form/TalentFormPage.tsx
+++ src/talents/components/form/TalentFormPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'reactstrap';
 import { RouteComponentProps } from 'react-router-dom';
 import TalentFormPageContainer from './TalentFormPageContainer';
-import './styles/TalentForm.module.css';
+import './styles/TalentForm.css';
 
 export default class TalentFormPage extends React.Component<RouteComponentProps> {
   render() {
