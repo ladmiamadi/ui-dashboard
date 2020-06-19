66325d361bc40d7e251da12efd56268285a618fd
AlexisChup <alexischup@gmail.com>
2020-06-19 09:35:04 +0200

    'Fix' CSS

--- src/app/components/homepage/Homepage.tsx
+++ src/app/components/homepage/Homepage.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { RootState } from '../../state/store';
 import { connect } from 'react-redux';
 import { Module } from '../../index.d';
-import classes from '../styles/Homepage.module.css';
+import './styles/Homepage.css';
 
 interface Props {
   modules: Module[],
@@ -13,14 +13,14 @@ export class Homepage extends React.Component<Props> {
   render() {
     return (
       <div>
-        <article className={classes.Container}>
+        <article className="container">
           <div className="section-top-border">
             <div className="row row-homepage">
               { this.props.modules.map((module, index) =>
                 <div className="col-md-4 col-sm-12 my-2" key={index}>
                   <div className="card">
-                    <div className={classes.CardBody}>
-                      <h5 className={classes.CardTitle}>{ module.name }</h5>
+                    <div className="card-body">
+                      <h5 className="card-title">{ module.name }</h5>
                       <p className="card-text">{ module.description }</p>
                       <Link to={module.link}>{ module.linkText }</Link>
                     </div>
