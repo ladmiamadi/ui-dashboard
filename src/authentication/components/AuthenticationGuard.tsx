74848f11a99572a8517c03ce3cdd6e60cfcbb1d1
antoine <antoine.toubeau@gmail.com>
2020-07-13 15:17:57 +0200

    redirect after 5 seconds to hdmnetwork.com when the token is not valid and add href to toastify

--- src/authentication/components/AuthenticationGuard.tsx
+++ src/authentication/components/AuthenticationGuard.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import { RootState, RootDispatch } from '../../app/state/store';
-import { Spinner } from 'reactstrap';
+import { toast } from 'react-toastify';
 
 interface OwnProps {
   children: React.ReactNode,
@@ -17,53 +16,43 @@ interface Props extends OwnProps {
 
 export class AuthenticationGuard extends React.Component<Props> {
   async componentDidMount() {
-
-    if (this.props.localToken && !this.props.isVerifiedToken) {
-      await this.props.verifyToken(this.props.localToken);
-      //setTimeout(this.redirectToHomePage, 5000);
+    try {
+      await this.props.verifyToken(this.props.localToken ? this.props.localToken : '');
+    } catch (error) {
+      toast.error(<div>You are no longer logged in the application. You will be redirected in 5 seconds.
+        <a href="https://www.hdmnetwork.com"> Click here to go back.</a> </div>);
+      setTimeout(() => {
+        /*you should comment next line to put the token while we don't have the authentication form
+        (we don't have time in 5 seconds) and you can uncomment after to test the redirection when
+        the token is incorrect or you can put more time for the timeout */
+        document.location.href = 'https://www.hdmnetwork.com';
+      }, 5000);
     }
   }
 
-  /*  async componentDidUpdate() {
-    if (this.props.localToken && !this.props.isVerifiedToken) {
+  async componentDidUpdate(prevProps: Props) {
+    if (this.props.token !== prevProps.token) {
       try {
-        await this.props.verifyToken(this.props.localToken);
+        await this.props.verifyToken(this.props.localToken ? this.props.localToken : '');
       } catch (error) {
-        setTimeout(this.redirectToHomePage, 5000);
+        toast.error(<div>You are no longer logged in the application. You will be redirected in 5 seconds.
+          <a href="https://www.hdmnetwork.com"> Click here to go back.</a> </div>);
+        setTimeout(() => {
+          /*you should comment next line to put the token while we don't have the authentication form
+          (we don't have time in 5 seconds) and you can uncomment after to test the redirection when
+          the token is incorrect or you can put more time for the timeout */
+          document.location.href = 'https://www.hdmnetwork.com';
+        }, 15000);
       }
     }
-  }*/
-
-  /*  redirectToHomePage = () => {
-    return <Redirect to="/"/>;
-  }*/
+  }
 
   render() {
-    console.log(this.props.token);
-    console.log(this.props.localToken);
-
-    if (this.props.localToken && !this.props.isVerifiedToken) {
-      console.log('spinner');
-      return <Spinner />;
-    }
-
-    if (this.props.localToken && !this.props.token) {
-      console.log('redirect');
-      //this.redirectToHomePage();
-      return <Redirect to="/"/>;
-    }
-
-    if (!this.props.token) {
-      return <div>you are not logged</div>;
-    }
-    /*    if (this.props.token) {
+    if (this.props.isVerifiedToken) {
       return this.props.children;
-    }*/
+    }
 
-    console.log('yes');
-    //setTimeout(this.redirectToHomePage, 5000);
-    console.log('null');
-    return this.props.children;
+    return <div></div>;
   }
 }
 
@@ -73,7 +62,7 @@ const mapState = (state: RootState) => ({
 });
 
 const mapDispatch = (dispatch: RootDispatch) => ({
-  verifyToken: dispatch.auth.verifyToken ,
+  verifyToken: dispatch.auth.verifyToken,
 });
 
 export default connect(mapState, mapDispatch)(AuthenticationGuard);
--- src/authentication/state/models/auth.ts
+++ src/authentication/state/models/auth.ts
@@ -1,53 +1,35 @@
 import { addTokenToRequestInterceptor, apiService, clearTokenFromAxios } from '../../../app/http/service';
 import { createModel } from '@rematch/core';
-import { Toastify } from '../../../helpers/Toastify';
-
-export interface UserAuthenticationDto {
-  username: string,
-  password: string,
-}
 
 interface AuthState {
-  isRequesting: boolean,
   isVerifiedToken: boolean,
   token: string | null,
 }
 
 export const auth = createModel({
   state: {
-    isRequesting: false,
     isVerifiedToken: false,
     token: null,
   },
   reducers: {
     clearToken: (state: AuthState): AuthState => ({ ...state, token: null }),
     setIsVerifiedToken: (state: AuthState, isVerifiedToken: boolean): AuthState => ({ ...state, isVerifiedToken }),
-    setIsRequesting: (state: AuthState, isRequesting: boolean): AuthState => ({ ...state, isRequesting }),
     updateToken: (state: AuthState, token: string): AuthState => ({ ...state, token }),
   },
   effects: {
     async verifyToken(token: string) {
-      try {
-        addTokenToRequestInterceptor(token);
+      addTokenToRequestInterceptor(token);
 
-        await apiService.post('api/token/verify', {});
+      await apiService.post('api/token/verify', {});
 
-        this.updateToken(token);
-      } catch (error) {
-        setTimeout(() => new Toastify().error('You are no longer logged in the application.'));
-        //new Toastify().error('You are no longer logged in the application.');
-        await clearTokenFromAxios();
-        this.clearToken();
-        //setTimeout(localStorage.clear, 5000);
-        localStorage.clear();
-      } finally {
-        this.setIsVerifiedToken(true);
-      }
+      this.updateToken(token);
+      this.setIsVerifiedToken(true);
     },
     async logout() {
       await clearTokenFromAxios();
       this.clearToken();
       localStorage.clear();
+      document.location.href = 'https://www.hdmnetwork.com';
     },
   },
 });
