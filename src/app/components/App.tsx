f01e833dfb58730ec763d9741e9aca24b6e8c582
Falaxir <falaxir@gmail.com>
2020-11-13 15:55:01 +0100

    Corrected conflict and timeline unable to fetch data

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -10,11 +10,7 @@ import { TalentsListPage } from '../../talents/components/TalentsListPage';
 import { store } from '../state/store';
 import RegisterUser from '../../talents/components/add-new-talent/modal/RegisterUser';
 import history from '../helpers/history';
-<<<<<<< HEAD
-import AuthenticationForm from '../../authentication/components/AuthenticationForm';
 import TimelineContainer from '../../timeline/components/TimelineContainer';
-=======
->>>>>>> d9003bf0be1a828561322ce8368efcfe45bd6468
 import './styles/App.css';
 
 export class App extends React.Component {
@@ -23,29 +19,14 @@ export class App extends React.Component {
       <Router history={history}>
         <Provider store={store}>
           <AppContainer>
-<<<<<<< HEAD
-            <AuthenticationGuard 
-              form={<AuthenticationForm />}
-              localToken={localStorage.getItem('hdm:admin:auth-token')}
-            >
-              <CustomNavbar/>
-              <Route path="/" exact component={Homepage} />
-              <Route path="/talent" exact component={TalentFormPage} />
-              <Route path="/talents" exact component= {TalentsListPage} />
-              <Route path="/editprofile" exact component={ProfileEditValidation} />
-              <Route path="/intern" exact component={RegisterUser} />
-              <Route path="/timeline" exact component={TimelineContainer} />
-            </AuthenticationGuard>
-          </AppContainer>                 
-=======
             <CustomNavbar/>
             <Route path="/" exact component={Homepage} />
             <Route path="/talent" exact component={TalentFormPage} />
             <Route path="/talents" exact component= {TalentsListPage} />
             <Route path="/editprofile" exact component={ProfileEditValidation} />
             <Route path="/intern" exact component={RegisterUser} />
+            <Route path="/timeline" exact component={TimelineContainer} />
           </AppContainer>
->>>>>>> d9003bf0be1a828561322ce8368efcfe45bd6468
         </Provider>
       </Router>
     );
