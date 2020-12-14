b1f1bd406ceb896b45ef2a724c632db11310590f
Jonathan Martin <jonathan.martin@epitech.eu>
2020-12-14 18:38:12 +0100

    fixed not kebab case css class

--- src/talents/constants/status-internship.ts
+++ src/talents/constants/status-internship.ts
@@ -1,18 +1,12 @@
 import { INTERNSHIP_STATUS } from '../../app/index.d';
 
-enum IS_FINISHING_ENUM {
-  'IS_FINISHING' = 'IS_FINISHING',
-}
-
-type UI_INTERNSHIP_STATUS = INTERNSHIP_STATUS | IS_FINISHING_ENUM;
-
-export const STATUS_INTERNSHIP_CLASS_NAMES: { [status in UI_INTERNSHIP_STATUS]: string } = {
-  [INTERNSHIP_STATUS.NON_STARTED]: 'notStarted-internship',
+export const STATUS_INTERNSHIP_CLASS_NAMES = {
+  [INTERNSHIP_STATUS.NON_STARTED]: 'not-started-internship',
   [INTERNSHIP_STATUS.ONGOING]: 'ongoing-internship',
   [INTERNSHIP_STATUS.ABANDONED]: 'abandoned-internship',
   [INTERNSHIP_STATUS.FINISHED]: 'finished-internship',
   [INTERNSHIP_STATUS.NONE]: 'none',
-  [IS_FINISHING_ENUM.IS_FINISHING]: 'is-finishing-internship',
+  ['IS_FINISHING']: 'is-finishing-internship',
 };
 
 export function getInternshipStatusClassname(
@@ -20,7 +14,7 @@ export function getInternshipStatusClassname(
   isInternshipFinishing: boolean): string
 {
   const statusInternship = status || INTERNSHIP_STATUS.NONE;
-  const uiStatusInternship = (isInternshipFinishing) ? IS_FINISHING_ENUM.IS_FINISHING : statusInternship;
+  const uiStatusInternship = (isInternshipFinishing) ? 'IS_FINISHING' : statusInternship;
 
   return STATUS_INTERNSHIP_CLASS_NAMES[uiStatusInternship];
 }
